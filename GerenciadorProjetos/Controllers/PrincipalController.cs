b6b3210d0c27aef6570c0c483a0c8603c463624a
FELIPE RIBEIRO ALVINO <11810965@acad.espm.br>
2018-09-28 16:42:43 -0300

    Primeiro commit

---
+++ GerenciadorProjetos/Controllers/PrincipalController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GerenciadorProjetos.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorProjetos.Controllers
+{
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public class PrincipalController : Controller
+    {
+
+        public string App_Data
+        {
+            get
+            {
+                return AppDomain.CurrentDomain.GetData("DataDirectory").ToString();
+            }
+        }
+
+        public IActionResult Erro(string mensagem)
+        {
+            return new ContentResult()
+            {
+                Content = mensagem,
+                ContentType = "text/plain",
+                StatusCode = 500
+            };
+        }
+
+        public string CaminhoFotos(int id)
+        {
+            return System.IO.Path.Combine(App_Data, "Fotos", id + ".jpg");
+        }
+    }
+}
